a1d4ebdb64d4ff90dcb5ba6ba82cbd48d39cd99c
Philip <phillip.emdokolo@student.moringaschool.com>
2024-03-25 14:20:35 +0300

    function types

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// For saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+  }
+  
+  // For mondayWork function
+  const mondayWork = function(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+  };
+  
+  // For wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return "You are " + flair + adjective + flair + "!";
+    };
+  }
+  
